3f6d8e0e2807a03e762a7b004771d892a9ad8e3b
mustafa mahmoud <31194057+mostafa-1224@users.noreply.github.com>
2020-02-06 03:36:23 +0200

    Updates

---
+++ precache-manifest.383c6e8f0e8989b4b3868dac494a9077.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02c4d750af3f7484118d3ef9b5a697f",
+    "url": "/brawl/index.html"
+  },
+  {
+    "revision": "e00dd590f170dd74c65a",
+    "url": "/brawl/static/css/main.eaa6ba9f.chunk.css"
+  },
+  {
+    "revision": "852774a698925d793951",
+    "url": "/brawl/static/js/2.1183de75.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/brawl/static/js/2.1183de75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00dd590f170dd74c65a",
+    "url": "/brawl/static/js/main.6ea51d89.chunk.js"
+  },
+  {
+    "revision": "7f8d827b768f1f09fd90",
+    "url": "/brawl/static/js/runtime-main.d6b15d37.js"
+  },
+  {
+    "revision": "b407e603e33951e15cb0f40b3b4de56b",
+    "url": "/brawl/static/media/brawlers.b407e603.jpg"
+  },
+  {
+    "revision": "b407e603e33951e15cb0f40b3b4de56b",
+    "url": "/brawl/static/media/brawlers1.b407e603.jpg"
+  },
+  {
+    "revision": "788b1461a8a3b286f6f9620ea0f27615",
+    "url": "/brawl/static/media/crow.788b1461.jpg"
+  },
+  {
+    "revision": "8ccb5655aaf6fbef87cf88a5c5588ed2",
+    "url": "/brawl/static/media/icon.8ccb5655.png"
+  },
+  {
+    "revision": "36ae396f1e0e5eed3ae17ebd43bf59ae",
+    "url": "/brawl/static/media/profile.36ae396f.jpg"
+  },
+  {
+    "revision": "d86f9f6fbd57e1d4897f812eb77bc94a",
+    "url": "/brawl/static/media/start.d86f9f6f.jpg"
+  }
+]);
